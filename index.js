06de4fe9de77af7c06bff4b51933a83947ea1711
Shreyas Hondadakatti <shreyashondadakatti@airasia.com>
2022-06-04 15:03:27 +0530

    [FIX] Make it work in iOS

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import ModelView from './lib/ModelView'
-import ARModelView from './lib/ARModelView'
 import Manager from './lib/Manager'
-import ARManager from './lib/ARManager'
 
 export {
   ModelView,
-  ARModelView,
   Manager,
-  ARManager
 }
